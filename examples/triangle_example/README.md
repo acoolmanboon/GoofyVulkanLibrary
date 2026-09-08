78dea4346fae23b2ab59f1b7b251eee7327a9bc5
acoolmanboon <acoolmanboon@gmail.com>
2026-09-08 10:29:34 +0700

    added TriangleExample
     it works but can't renderdoc it for some reason. Something broken

---
+++ examples/triangle_example/README.md
@@ -0,0 +1,19 @@
+# GFVL Moon Example
+This example is a simple procedurally-generated terrain resembling the moon or some generic rocky planet.
+This uses the Perlin Noise library made by  Ryo Suzuki <reputeless@gmail.com>
+
+***GFVL doesn't have that many features yet so this uses pretty much every feature available.***
+
+## How to compile
+
+- Build GFVL but add option COMPILE_MOON_EXAMPLE (or COMPILE_ALL_EXAMPLES to also compile every other example.)
+- Compile the files as usual
+- Compile the GLSL fragment and vertex shaders into SPIR-V (.spv) files. Make sure that they are in the same directory.
+- Run the .exe, it should be in this directory.
+
+## Controls
+- **V** to fly
+- **Escape** to unlock mouse
+- **Left Shift** to run
+- **Space** to move the light source to your position
+- **WASD** or **ARROW KEYS** for movement
---
+++ examples/triangle_example/main.cpp
@@ -0,0 +1,85 @@
+// this is just an example script so use it anywhere
+
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <stdexcept>
+
+#include <GFVL.hpp>
+#include <vulkan/vulkan_core.h>
+
+// This defines how your vertices will work in GFVL.
+// Later in the script, the structure of vertice will be defined to GFVL.
+struct vertice {
+  float position[2];
+  float color[3];
+};
+
+int main() {
+  // This is where you tell GFVL where your shaders are. You need to compile the shader languages first.
+  // I know that there are more shader stages available but for now I haven't learned that yet so I am only certain that it supports only a vertex and fragment shader for now.
+  std::vector<GFVL::ShaderStage> shaderStages = {
+      {.flags = VK_SHADER_STAGE_VERTEX_BIT,
+       .filename = "vertex_shader.vert.spv"},
+      {.flags = VK_SHADER_STAGE_FRAGMENT_BIT,
+       .filename = "fragment_shader.frag.spv"}};
+
+  std::vector<GFVL::UniformBufferBinding> bindings;
+  // Now we define the layout of the "vertice" struct to GFVL.
+  GFVL::VertexLayout layout = {
+    .bindings = {
+     {
+      .binding = 0,
+      .stride = static_cast<uint32_t>(sizeof(vertice)),
+      .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
+     } 
+    },
+    .attributes = {
+      {
+        .location = 0,
+        .binding = 0,
+        .format = VK_FORMAT_R32G32_SFLOAT,
+        .offset = offsetof(vertice, position)
+      },
+      {
+        .location = 1,
+        .binding = 0,
+        .format = VK_FORMAT_R32G32B32_SFLOAT,
+        .offset = offsetof(vertice, color)  
+      }
+    }
+  };
+
+  // This defines our actual application information.
+  GFVL::AppInfo appInfo = {
+      .applicationName = "Hello, Triangle!", // For now, this just sets the window name. 
+      .applicationVersion = 1, // Arbitrary version number
+      .width = 800, // The starting width of the window.
+      .height = 600, // The starting height of the window
+      .preferredGPU = GFVL::PreferredGPU::Performance}; 
+
+  GFVL::Instance GFVLinstance(appInfo, layout, bindings, shaderStages); // With our initialization logic done, we create the instance.
+
+  std::vector<vertice> triangle = {
+      {.position = {0.0f, -0.5f}, .color = {1.0f, 0.0f, 0.0f}},
+      {.position = {-0.5f, 0.5f}, .color = {0.0f, 1.0f, 0.0f}},
+      {.position = {0.5f, 0.5f}, .color = {0.0f, 0.0f, 1.0f}},
+  };
+
+  GFVL::Mesh triangleMesh = GFVLinstance.createMesh(GFVL::Mesh::CreateInfo{
+      .verticeDataSize = triangle.size() * sizeof(vertice),
+      .verticeCount = static_cast<uint32_t>(triangle.size()),
+      .verticeData = triangle.data(),
+      .indiceType = GFVL::Mesh::IndiceDataType::NotDefined,
+      .memoryAllocation = GFVL::MeshBuffer::MemoryAllocation::DeviceOnly});
+
+  while (GFVLinstance.inputState.isRunning()) { // Make sure to encase the while loop in the .isRunning check.
+    GFVLinstance.inputState.pollInputs(); // We need to call pollInputs first in every frame to 
+
+    GFVLinstance.beginFrame();
+    GFVLinstance.renderMesh(triangleMesh);
+    GFVLinstance.endFrame();
+  }
+
+  return 0;
+}
